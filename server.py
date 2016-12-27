a980430c8997ecc8baf8bc0731909dab22b96b07
Daniel Bercht <d.bercht@gmail.com>
2016-12-26 23:12:35 -0500

    add uwsgi wrapper

---
+++ server.py
@@ -0,0 +1,42 @@
+import os
+
+import tornado.ioloop
+import tornado.web
+
+from nlp.luNlp import analyze_wod
+from nlp.trackable import Builder
+
+class MainHandler(tornado.web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+        self.set_header('Access-Control-Allow-Headers', 'Content-Type')
+
+    def get(self):
+        self.write({'status': 'ok'})
+
+    def post(self):
+        wod = self.request.body
+        if not wod:
+          raise tornado.web.HTTPError(400)
+        result = analyze_wod(wod)
+        b = Builder.build(result)
+        self.set_header('Content-Type', 'application/json')
+        self.write({
+          'input': wod,
+          'output': b.todict(),
+        })
+
+    def options(self):
+        self.set_status(204)
+        self.finish()
+
+def application():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+if __name__ == "__main__":
+    app = application()
+    app.listen(os.environ.get('PORT', 8888))
+    tornado.ioloop.IOLoop.current().start()
