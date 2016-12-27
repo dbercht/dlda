e5b0131b0f15efe5b6c78e60a4cc6af69e57fb49
Daniel Bercht <d.bercht@gmail.com>
2016-12-26 23:33:55 -0500

    flaskify application.py

--- application.py
+++ application.py
@@ -1,4 +1,28 @@
-import tornado.wsgi
-from server import application
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
-application = tornado.wsgi.WSGIAdapter(application)
+from nlp.luNlp import analyze_wod
+from nlp.trackable import Builder
+
+application = Flask(__name__)
+CORS(application)
+
+@application.route("/", methods=['GET'])
+def get():
+    return jsonify(status='ok')
+
+@application.route("/", methods=['post'])
+def post():
+    wod = request.get_json()
+    if not wod:
+      return jsonify(error='No wod sent'), 400
+    result = analyze_wod(wod)
+    b = Builder.build(result)
+    result = {
+      'input': wod,
+      'output': b.todict(),
+    }
+    return jsonify(**result)
+
+if __name__ == '__main__':
+     application.run(host= '0.0.0.0', debug=True)
