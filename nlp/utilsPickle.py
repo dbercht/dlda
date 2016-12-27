fc1200ca0a08949f9ed15493fba336223fab6340
Daniel Bercht <d.bercht@gmail.com>
2016-12-27 00:35:29 -0500

    memoization of taggers/chunkers

--- nlp/utilsPickle.py
+++ nlp/utilsPickle.py
@@ -1,17 +1,20 @@
 # NOTE: Unsafe assumption that all pickles are in current dir
 
+pickles = {}
 #Dumps a pickle
 def pickleDump(tagger, filename):
-    from cPickle import dump
-    output = open('./pickles/' + filename + '.pkl', 'wb')
-    dump(tagger, output, -1)
-    output.close()
+    pickles[filename] = tagger
+    #  from cPickle import dump
+    #  output = open('./pickles/' + filename + '.pkl', 'wb')
+    #  dump(tagger, output, -1)
+    #  output.close()
 
 #Eats a pickle
 def pickleLoad(filename):
-  from cPickle import load
-  filename += '.pkl'
-  input = open('./pickles/' + filename, 'rb')
-  var = load(input)
-  input.close()
-  return var
+  return pickles[filename]
+  #  from cPickle import load
+  #  filename += '.pkl'
+  #  input = open('./pickles/' + filename, 'rb')
+  #  var = load(input)
+  #  input.close()
+  #  return var
